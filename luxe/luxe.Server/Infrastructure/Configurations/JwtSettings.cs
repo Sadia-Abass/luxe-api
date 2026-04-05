57d360cbe4a19b25d12ac3d85e279e9d1583099d
Sadia Abass <sadia.abass@outlook.com>
2026-04-05 19:55:53 +0100

    Add jwt settings class with required properties.

---
+++ luxe/luxe.Server/Infrastructure/Configurations/JwtSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace luxe.Server.Infrastructure.Configurations
+{
+    public class JwtSettings
+    {
+        public string SecretKey { get; set; } = null!;
+        public string Issuer { get; set; } = null!;
+        public string Audience { get; set; } = null!;
+        public int MinutesToExpiration { get; set; }
+        public int RefreshTokenExpirationDays { get; set; }
+    }
+}
