ee4eda176220dc4ffe2f759c0d14663f85061ced
Sadia Abass <sadia.abass@outlook.com>
2026-04-06 16:56:43 +0100

    Added jwt configuration setting.

--- luxe/luxe.Server/Infrastructure/Configurations/ApplicationServicesConfigurationsExtension.cs
+++ luxe/luxe.Server/Infrastructure/Configurations/ApplicationServicesConfigurationsExtension.cs
@@ -1,6 +1,11 @@
 ﻿using luxe.Server.Domain.Entities;
 using luxe.Server.Infrastructure.Data;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+
 namespace luxe.Server.Infrastructure.Configurations
 {
     public static class ApplicationServicesConfigurationsExtension
@@ -24,8 +29,50 @@ namespace luxe.Server.Infrastructure.Configurations
                 options.Password.RequireUppercase = true;
                 options.Password.RequireNonAlphanumeric = false;
                 options.Password.RequiredLength = 8;
-            });
 
+                options.User.RequireUniqueEmail = true;
+                options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+
+                options.SignIn.RequireConfirmedEmail = true;
+                options.SignIn.RequireConfirmedPhoneNumber = false;
+            }).AddEntityFrameworkStores<AppDbContext>()
+            .AddDefaultTokenProviders()
+            .AddTokenProvider<DataProtectorTokenProvider<AppUser>>("RefreshTokenProvider");
+
+
+            var jwtSettings = configuration.GetSection("JwtSettings").Get<JwtSettings>();
+            if(jwtSettings != null || string.IsNullOrEmpty(jwtSettings.SecretKey))
+            {
+                throw new InvalidOperationException("jwt secret key is not configured properly");
+            }
+
+            services.AddAuthentication(options => 
+            { 
+                options.DefaultAuthenticateScheme = 
+                options.DefaultChallengeScheme =
+                options.DefaultForbidScheme =
+                options.DefaultScheme =
+                options.DefaultSignInScheme =
+                options.DefaultSignOutScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.SaveToken = true;
+                options.RequireHttpsMetadata = false;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = jwtSettings.Issuer,
+                    ValidAudience = jwtSettings.Audience,
+                    IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(jwtSettings.SecretKey)),
+                    ClockSkew = TimeSpan.Zero,
+                    RoleClaimType = ClaimTypes.Role,
+                    NameClaimType = ClaimTypes.Name
+                };
+            });
+            services.AddAuthorization();
 
             return services;
         }
