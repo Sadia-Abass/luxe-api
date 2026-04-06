55131565b19ea33c757aeaf022fab6016b37ecda
Sadia Abass <sadia.abass@outlook.com>
2026-04-06 16:31:50 +0100

    Added seed data for approles.

---
+++ luxe/luxe.Server/Infrastructure/Data/SeedData.cs
@@ -0,0 +1,22 @@
+﻿using luxe.Server.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace luxe.Server.Infrastructure.Data
+{
+    public static class SeedData
+    {
+        public static void Seed(this ModelBuilder modelBuilder)
+        {
+            List<AppRole> appRoles = new List<AppRole> 
+            {
+                new AppRole{Id = Guid.NewGuid(), Name = "Super Admin", Description = "", NormalizedName = "SUPER ADMIN", DateCreated = DateTime.UtcNow, IsActive = true},
+                new AppRole{Id = Guid.NewGuid(), Name = "Admin", Description = "", NormalizedName = "ADMIN", DateCreated = DateTime.UtcNow, IsActive = true},
+                new AppRole{Id = Guid.NewGuid(), Name = "Manager", Description = "", NormalizedName = "MANAGER", DateCreated = DateTime.UtcNow, IsActive = true},
+                new AppRole{Id = Guid.NewGuid(), Name = "Customer Service", Description = "", NormalizedName = "CUSTOMER SERVICE", DateCreated = DateTime.UtcNow, IsActive = true},
+                new AppRole{Id = Guid.NewGuid(), Name = "User", Description = "", NormalizedName = "USER", DateCreated = DateTime.UtcNow, IsActive = true},
+            };
+            modelBuilder.Entity<AppRole>().HasData(appRoles);
+        }
+        
+    }
+}
