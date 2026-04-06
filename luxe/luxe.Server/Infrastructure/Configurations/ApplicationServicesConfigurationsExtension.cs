df7aea53ee94fdb44821db022aa40384563429c9
Sadia Abass <sadia.abass@outlook.com>
2026-04-06 12:10:40 +0100

    Added appication services cnfiguration extension with sql server configuration.

---
+++ luxe/luxe.Server/Infrastructure/Configurations/ApplicationServicesConfigurationsExtension.cs
@@ -0,0 +1,24 @@
+﻿using luxe.Server.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace luxe.Server.Infrastructure.Configurations
+{
+    public static class ApplicationServicesConfigurationsExtension
+    {
+        public static IServiceCollection AddApplicationServicesConfigurations(this IServiceCollection services, IConfiguration configuration)
+        {
+            // Add application services configurations here
+            // Configure services, options, etc. based on the application's needs
+            //Congigure EF Core with SQL Server
+            var connectionString = configuration.GetConnectionString("luxeDB") ?? throw new ArgumentNullException(nameof(configuration));
+
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(connectionString);
+            });
+            
+                
+            return services;
+        }
+    }
+}
