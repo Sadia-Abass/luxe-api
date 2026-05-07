c016efd965616669dcea5cb20e799da7f9e61242
Sadia Abass <sadia.abass@outlook.com>
2026-05-07 15:41:14 +0100

    Added assigned role dto to project.

---
+++ luxe/luxe.Server/Application/DTOs/AuthenticationDTOs/AssignRoleDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace luxe.Server.Application.DTOs.AuthenticationDTOs
+{
+    public class AssignRoleDTO
+    {
+        public string UserName { get; set; } = string.Empty;
+        public string RoleName { get; set; } = string.Empty;
+    }
+}
