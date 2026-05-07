923cab228d0a1edd28b10993ac529e5f73f422f0
Sadia Abass <sadia.abass@outlook.com>
2026-05-07 15:41:34 +0100

    Added token response dto.

---
+++ luxe/luxe.Server/Application/DTOs/AuthenticationDTOs/TokenResponseDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace luxe.Server.Application.DTOs.AuthenticationDTOs
+{
+    public class TokenResponseDTO
+    {
+        public string AccessToken { get; set; } = string.Empty;
+        public string RefreshToken { get; set; } = string.Empty;
+    }
+}
