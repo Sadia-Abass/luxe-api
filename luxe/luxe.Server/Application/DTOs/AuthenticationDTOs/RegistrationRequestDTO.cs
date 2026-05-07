ad9d2b96514e2c6d0c6833111fb9d8f5db89bb87
Sadia Abass <sadia.abass@outlook.com>
2026-05-07 13:11:26 +0100

    Add registration request dto.

---
+++ luxe/luxe.Server/Application/DTOs/AuthenticationDTOs/RegistrationRequestDTO.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace luxe.Server.Application.DTOs.AuthenticationDTOs
+{
+    public class RegistrationRequestDTO
+    {
+        [Required(ErrorMessage = "First Name is required.")]
+        [StringLength(50, ErrorMessage = "First Name cannot be longer than 50 characters.")]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Last Name is required.")]
+        [StringLength(50, ErrorMessage = "Last Name cannot be longer than 50 characters.")]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email address.")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Password is required.")]
+        [StringLength(100, MinimumLength = 8, ErrorMessage = "Password must be at least 8 characters long.")]
+        public string? Password { get; set; }
+    }
+}
