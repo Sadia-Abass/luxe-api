9f1e39c074520c9505c8c609da8de1987e20095f
Sadia Abass <sadia.abass@outlook.com>
2026-05-07 16:11:17 +0100

    Rename token service and completed implementation.

---
+++ luxe/luxe.Server/Application/Services/ITokenService.cs
@@ -0,0 +1,13 @@
+﻿using luxe.Server.Application.DTOs;
+using luxe.Server.Application.DTOs.AuthenticationDTOs;
+using luxe.Server.Domain.Entities;
+
+namespace luxe.Server.Application.Services
+{
+    public interface ITokenService
+    {
+        // Task<ApiResponse<>> RegisterAsync(RegistrationRequestDTO registrationRequestDTO);
+        Task<string> CreateAccessTokenAsync(AppUser user, IList<string> roles);
+        Task<RefreshToken> CreateRefreshToken(string ipAddress);   
+    }
+}
---
+++ luxe/luxe.Server/Infrastructure/Services/TokenService.cs
@@ -0,0 +1,69 @@
+﻿using luxe.Server.Application.Services;
+using luxe.Server.Domain.Entities;
+using luxe.Server.Infrastructure.Configurations;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace luxe.Server.Infrastructure.Services
+{
+    public class TokenService : ITokenService
+    {
+        private readonly IConfiguration _configuration;
+
+        public TokenService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public Task<string> CreateAccessTokenAsync(AppUser user, IList<string> roles)
+        {
+            var jwtSettings = _configuration.GetSection("JwtSettings").Get<JwtSettings>();
+            var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.SecretKey));
+            var credentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256); 
+
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.Id),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email ?? ""),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(ClaimTypes.NameIdentifier, user.Id),
+            };
+
+            foreach(var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+
+            var token = new JwtSecurityToken(
+                issuer: jwtSettings.Issuer,
+                audience: jwtSettings.Audience,
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(jwtSettings.MinutesToExpiration),
+                signingCredentials: credentials);
+
+            //var tokenHandler = new JwtSecurityTokenHandler();
+            //var tokenString = tokenHandler.WriteToken(token);
+            //return Task.FromResult(tokenString);
+
+            return Task.FromResult(new JwtSecurityTokenHandler().WriteToken(token));
+        }
+
+        public Task<RefreshToken> CreateRefreshToken(string ipAddress)
+        {
+            var jwtSettings = _configuration.GetSection("JwtSettings").Get<JwtSettings>();
+            var randomBytes = new byte[64];
+            using var random = RandomNumberGenerator.Create();
+
+            return Task.FromResult(new RefreshToken
+            {
+                Token = Convert.ToBase64String(randomBytes),
+                Expires = DateTime.UtcNow.AddDays(jwtSettings.RefreshTokenExpirationDays),
+                CreatedDate = DateTime.UtcNow,
+                CreatedByIp = ipAddress
+            });
+        }
+    }
+}
