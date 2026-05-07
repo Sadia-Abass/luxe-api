2a548897293a964a8c0103d84eb7cd212c85062e
Sadia Abass <sadia.abass@outlook.com>
2026-05-07 23:09:49 +0100

    Added authentication controller.

---
+++ luxe/luxe.Server/API/Controllers/AuthenticationController.cs
@@ -0,0 +1,67 @@
+﻿using luxe.Server.Application.DTOs.AuthenticationDTOs;
+using luxe.Server.Application.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace luxe.Server.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly IAuthenticationRepository _authenticationRepository;
+
+        public AuthenticationController(IAuthenticationRepository authenticationRepository)
+        {
+            _authenticationRepository = authenticationRepository;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegistrationRequestDTO registerRequestDto)
+        {
+            var result = await _authenticationRepository.RegisterAsync(registerRequestDto);
+            if (!result.IsSuccess)
+            {
+                return BadRequest(result);
+            }
+
+            return Ok(result);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequestDTO loginRequestDto)
+        {
+            var result = await _authenticationRepository.LoginAsync(loginRequestDto);
+            if (!result.IsSuccess)
+            {
+                return Unauthorized(result);
+            }
+
+            return Ok(result);
+        }
+
+        [HttpPost("refresh-token")]
+        public async Task<IActionResult> RefreshToken([FromBody] TokenRequestDTO tokenRequestDto)
+        {
+            var result = await _authenticationRepository.RefreshTokenAsync(tokenRequestDto);
+            if (!result.IsSuccess)
+            {
+                return NotFound(result);
+            }
+
+            return Ok(result);
+        }
+
+        [HttpPost("revoke")]
+        public async Task<IActionResult> RevokeToken([FromBody] TokenRequestDTO tokenRequestDto)
+        {
+            var result = await _authenticationRepository.RevokeTokenAsync(tokenRequestDto);
+            if (!result.IsSuccess)
+            {
+                return NotFound(result);
+            }
+
+            return Ok(result);
+        }
+    }
+}
