9901d125b3d26bca070dc68c499e7996ed362cd0
Sadia Abass <sadia.abass@outlook.com>
2026-05-07 15:36:54 +0100

    Added token request dto.

---
+++ luxe/luxe.Server/Application/DTOs/AuthenticationDTOs/TokenRequestDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace luxe.Server.Application.DTOs.AuthenticationDTOs
+{
+    public class TokenRequestDTO
+    {
+        public string? AccessToken { get; set; }
+        public string? RefreshToken { get; set; }
+    }
+}
