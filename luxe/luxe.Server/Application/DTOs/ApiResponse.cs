5bcd42617a15c19a4890e1e9fdef5eea92b634fb
Sadia Abass <sadia.abass@outlook.com>
2026-05-07 13:41:44 +0100

    Add api response class with required properties.

---
+++ luxe/luxe.Server/Application/DTOs/ApiResponse.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+
+namespace luxe.Server.Application.DTOs
+{
+    public class ApiResponse<T>
+    {
+        public HttpStatusCode StatusCode { get; set; }
+        public bool IsSuccess { get; set; }
+        public List<string> ErrorMessages { get; set; } = new List<string>();
+        public T? Data { get; set; }
+    }
+}
