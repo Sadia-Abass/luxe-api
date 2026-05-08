5cffd32c265a3fdfcd0f9846d15ffd8092d2b826
Sadia Abass <sadia.abass@outlook.com>
2026-05-08 21:34:11 +0100

    Started adding the navbar implementation.

--- luxe/luxe.client/src/components/layout/Header.jsx
+++ luxe/luxe.client/src/components/layout/Header.jsx
@@ -1,10 +1,49 @@
+import { NavLink } from "react-router-dom";
+import { ROUTES } from "../../util/constants";
+
 function Header() {
   return (
-    <>
-      <div>
-        <h1>Header</h1>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarTogglerDemo01"
+          aria-controls="navbarTogglerDemo01"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+          <NavLink className="navbar-brand" href="#">
+            Hidden brand
+          </NavLink>
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <NavLink
+                to={ROUTES.HOME}
+                className="nav-link active"
+                aria-current="page"
+              >
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to={ROUTES.LOGIN} className="nav-link">
+                Link
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to={ROUTES.REGISTER} className="nav-link">
+                Register
+              </NavLink>
+            </li>
+          </ul>
+        </div>
       </div>
-    </>
+    </nav>
   );
 }
 
