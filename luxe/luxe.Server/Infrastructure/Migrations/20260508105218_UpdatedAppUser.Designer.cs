06d3ea20e7dc9fc5bd8f3a3f410d27bb44957a69
Sadia Abass <sadia.abass@outlook.com>
2026-05-08 11:55:57 +0100

    Added migration for changes made.

--- luxe/luxe.Server/Infrastructure/Migrations/20260508105218_UpdatedAppUser.Designer.cs
+++ luxe/luxe.Server/Infrastructure/Migrations/20260508105218_UpdatedAppUser.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using luxe.Server.Infrastructure.Data;
 
@@ -11,9 +12,11 @@ using luxe.Server.Infrastructure.Data;
 namespace luxe.Server.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260508105218_UpdatedAppUser")]
+    partial class UpdatedAppUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ luxe/luxe.Server/Infrastructure/Migrations/20260508105218_UpdatedAppUser.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace luxe.Server.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatedAppUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Token",
+                schema: "dbo",
+                table: "RefreshTokens",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Token",
+                schema: "dbo",
+                table: "RefreshTokens",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
