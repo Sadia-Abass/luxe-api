948a0cba4c74255b316ade26f03e070c9a4b01fe
Sadia Abass <sadia.abass@outlook.com>
2026-05-08 21:33:30 +0100

    Added more constants to Routes constant.

--- luxe/luxe.client/src/util/constants.js
+++ luxe/luxe.client/src/util/constants.js
@@ -2,4 +2,9 @@ export const ROUTES = {
   HOME: "/",
   REGISTER: "/register",
   LOGIN: "/login",
+  CART: "/cart",
+  CHECKOUT: "/checkout",
+  PRODUCT_DETAIL: "/product/:id",
+  PRODUCT_MANAGEMENT: "/product-management",
+  ORDER_MANAGEMENT: "/order-management",
 };
