ab8fc3cbf76f81aee19ada31e417c2cf440f1509
Sadia Abass <sadia.abass@outlook.com>
2026-06-02 08:37:22 +0100

    Added subcategories and properties.

---
+++ luxe/luxe.Server/Domain/Entities/Subcategory.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace luxe.Server.Domain.Entities
+{
+    public class Subcategory : BaseEntity
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; } = null!;
+    }
+}
