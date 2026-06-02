2228919d8f63b051be8a4fc6700dc5faae159038
Sadia Abass <sadia.abass@outlook.com>
2026-06-02 09:16:38 +0100

    Added category dto class and properties.

---
+++ luxe/luxe.Server/Application/DTOs/Category/CategoryDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace luxe.Server.Application.DTOs.Category
+{
+    public class CategoryDTO
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Name is required.")]
+        public string Name { get; set; } = string.Empty;
+    }
+}
