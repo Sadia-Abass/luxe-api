f78dabae92844c025fd30da96ef9eb085b8127de
Sadia Abass <sadia.abass@outlook.com>
2026-06-02 08:36:41 +0100

    Added base entity class to have other classes inherit it for the most common needed properties.

---
+++ luxe/luxe.Server/Domain/Entities/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace luxe.Server.Domain.Entities
+{
+    public class BaseEntity
+    {
+        public DateTime CreatedDate { get; set; }
+        public DateTime LastUpdatedDate { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
