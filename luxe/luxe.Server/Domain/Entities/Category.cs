3dec8def6f89c7827ca1265cdcb98f90ed3f15a5
Sadia Abass <sadia.abass@outlook.com>
2026-06-02 08:37:01 +0100

    Added category class and properties.

---
+++ luxe/luxe.Server/Domain/Entities/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace luxe.Server.Domain.Entities
+{
+    public class Category: BaseEntity
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public ICollection<Subcategory> Subcategories { get; set; } = new List<Subcategory>();
+    }
+}
