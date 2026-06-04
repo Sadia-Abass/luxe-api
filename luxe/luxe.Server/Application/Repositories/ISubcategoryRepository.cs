a26b52889ba4c23ae6c694b3f42e8e6f31bfd4b1
Sadia Abass <sadia.abass@outlook.com>
2026-06-04 15:32:04 +0100

    Added subcategory interface.

---
+++ luxe/luxe.Server/Application/Repositories/ISubcategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using luxe.Server.Application.DTOs;
+using luxe.Server.Application.DTOs.Subcategory;
+
+namespace luxe.Server.Application.Repositories
+{
+    public interface ISubcategoryRepository
+    {
+        Task<ApiResponse<SubcategoryDTO>> GetSubcategoryByIdAsync(int id);
+        Task<ApiResponse<IEnumerable<SubcategoryDTO>>> GetAllSubcategoriesAsync();
+        Task<ApiResponse<SubcategoryDTO>> CreateSubcategoryAsync(CreateSubcategoryDTO createSubcategoryDTO);
+        Task<ApiResponse<SubcategoryDTO>> UpdateSubcategoryAsync(UpdateSubcategoryDTO updateSubcategoryDTO);
+        Task<ApiResponse<bool>> DeleteSubcategoryAsync(int id);
+    }
+}
