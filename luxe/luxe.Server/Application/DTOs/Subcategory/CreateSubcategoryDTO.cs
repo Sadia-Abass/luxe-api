1b739b1dc43146ede3628e581ebfd9c8d70605fa
Sadia Abass <sadia.abass@outlook.com>
2026-06-04 15:26:52 +0100

    Added create subcategory dto.

---
+++ luxe/luxe.Server/Application/DTOs/Subcategory/CreateSubcategoryDTO.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace luxe.Server.Application.DTOs.Subcategory
+{
+    public class CreateSubcategoryDTO
+    {
+        [Required(ErrorMessage = "Name is required.")]
+        public string Name { get; set; } = string.Empty;
+    }
+}
