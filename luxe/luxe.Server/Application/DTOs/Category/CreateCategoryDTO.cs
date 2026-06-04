d0a1b21dd04c78605e614e09dc76f2122bd754ed
Sadia Abass <sadia.abass@outlook.com>
2026-06-04 15:25:26 +0100

    Added create category dto.

---
+++ luxe/luxe.Server/Application/DTOs/Category/CreateCategoryDTO.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace luxe.Server.Application.DTOs.Category
+{
+    public class CreateCategoryDTO
+    {
+        [Required(ErrorMessage = "Name is required.")]
+        public string Name { get; set; } = string.Empty;
+    }
+}
