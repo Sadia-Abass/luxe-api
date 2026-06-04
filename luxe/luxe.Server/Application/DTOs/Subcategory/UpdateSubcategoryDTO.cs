3cdcdf4c66fbdacee8eb14b6135ea9827032076e
Sadia Abass <sadia.abass@outlook.com>
2026-06-04 15:27:21 +0100

    Added sebcategory dto.

---
+++ luxe/luxe.Server/Application/DTOs/Subcategory/UpdateSubcategoryDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace luxe.Server.Application.DTOs.Subcategory
+{
+    public class UpdateSubcategoryDTO
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Name is required.")]
+        public string Name { get; set; } = string.Empty;
+    }
+}
