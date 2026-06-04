292b75f0a6b89e9f8d84f26511fa74d441c4625e
Sadia Abass <sadia.abass@outlook.com>
2026-06-04 15:31:43 +0100

    Added category repository interface.

---
+++ luxe/luxe.Server/Application/Repositories/ICategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using luxe.Server.Application.DTOs;
+using luxe.Server.Application.DTOs.Category;
+
+namespace luxe.Server.Application.Repositories
+{
+    public interface ICategoryRepository
+    {
+        Task<ApiResponse<CategoryDTO>> GetCategoryByIdAsync(int id);
+        Task<ApiResponse<IEnumerable<CategoryDTO>>> GetAllCategoriesAsync();
+        Task<ApiResponse<CategoryDTO>> CreateCategoryAsync(CreateCategoryDTO createCategoryDTO);
+        Task<ApiResponse<CategoryDTO>> UpdateCategoryAsync(UpdateCategoryDTO updateCategoryDTO);
+        Task<ApiResponse<bool>> DeleteCategoryAsync(int id);
+    }
+}
