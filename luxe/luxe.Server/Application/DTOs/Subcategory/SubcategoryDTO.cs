0cc8d0f06b28c20d18336fb3f5e8f6fabeb6d93d
Sadia Abass <sadia.abass@outlook.com>
2026-07-01 23:37:20 +0100

    Add categoryId property to subcategoryDto.

--- luxe/luxe.Server/Application/DTOs/Subcategory/SubcategoryDTO.cs
+++ luxe/luxe.Server/Application/DTOs/Subcategory/SubcategoryDTO.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using luxe.Server.Application.DTOs.Category;
+using System.ComponentModel.DataAnnotations;
 
 namespace luxe.Server.Application.DTOs.Subcategory
 {
@@ -7,6 +8,7 @@ namespace luxe.Server.Application.DTOs.Subcategory
         public int Id { get; set; }
         [Required(ErrorMessage = "Name is required.")]
         public string Name { get; set; } = string.Empty;
+        public int CategoryId { get; set; }
         public string CategoryName { get; set; } = string.Empty;
     }
 }
