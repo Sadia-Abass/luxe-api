69f3ce65ddd693b16d3c50164cd200b2cc80d0e4
Sadia Abass <sadia.abass@outlook.com>
2026-07-01 20:47:23 +0100

    Add categoryName property to subcategoryDto.

--- luxe/luxe.Server/Application/DTOs/Subcategory/SubcategoryDTO.cs
+++ luxe/luxe.Server/Application/DTOs/Subcategory/SubcategoryDTO.cs
@@ -7,5 +7,6 @@ namespace luxe.Server.Application.DTOs.Subcategory
         public int Id { get; set; }
         [Required(ErrorMessage = "Name is required.")]
         public string Name { get; set; } = string.Empty;
+        public string CategoryName { get; set; } = string.Empty;
     }
 }
