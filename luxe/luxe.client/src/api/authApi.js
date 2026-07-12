14d8515acccc962e23ad117571c928632e952dba
Sadia Abass <sadia.abass@outlook.com>
2026-07-13 00:07:31 +0100

    Update authApi

--- luxe/luxe.client/src/api/authApi.js
+++ luxe/luxe.client/src/api/authApi.js
@@ -1,12 +1,14 @@
 import { apiFetchJson } from "./apiFetch";
+const BASE_URL = import.meta.env.VITE_API_URL;
 
 function buildQuery(params) {
   return new URLSearchParams(params).toString();
 }
 
 export const login = (email, password) =>
-  apiFetchJson("/authentication/login", {
+  fetch(`${BASE_URL}/authentication/login`, {
     method: "POST",
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ email, password }),
   });
 
