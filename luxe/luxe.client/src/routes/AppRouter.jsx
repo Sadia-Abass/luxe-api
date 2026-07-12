dcbfe738e715c6ad1e499ab711288d2556aa43db
Sadia Abass <sadia.abass@outlook.com>
2026-07-13 00:09:15 +0100

    Add login and dasboard routes

--- luxe/luxe.client/src/routes/AppRouter.jsx
+++ luxe/luxe.client/src/routes/AppRouter.jsx
@@ -1,11 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import { ROUTES } from "../util/constants";
+import Login from "../pages/auth/Login";
+import Dashboard from "../pages/Dashboard";
 
 function AppRouter() {
   return (
     <Routes>
       <Route path={ROUTES.HOME} element={<Home />} />
+      <Route path={ROUTES.LOGIN} element={<Login />} />
+      <Route path={ROUTES.DASHBOARD} element={<Dashboard />} />
       {/* <Route path="" element={} /> */}
     </Routes>
   );
