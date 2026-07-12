1241a24aed14ed0a91af4cde7a94d7f5a63f44a8
Sadia Abass <sadia.abass@outlook.com>
2026-07-12 23:25:16 +0100

    Add import for AuthProvider.

--- luxe/luxe.client/src/main.jsx
+++ luxe/luxe.client/src/main.jsx
@@ -4,6 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
 import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthContext.jsx";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
