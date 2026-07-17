d5d19c2b9a80e90458ea5f1e4b5583e859edcdbf
Sadia Abass <sadia.abass@outlook.com>
2026-07-17 23:54:24 +0100

    Add IUserRepository interface and add SaveRefreshTokenAsync method to it.

---
+++ luxe/luxe.Server/Application/Repositories/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using luxe.Server.Domain.Entities;
+
+namespace luxe.Server.Application.Repositories
+{
+    public interface IUserRepository
+    {
+        Task SaveRefreshTokenAsync(RefreshToken refreshToken);
+    }
+}
