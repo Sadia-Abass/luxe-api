99e536332b1af3a11be25b13b3cb79f7c2cf7f2e
Sadia Abass <sadia.abass@outlook.com>
2026-07-17 23:37:26 +0100

    Add GetPrincipleFromExpiredToken method to ITokenService interface.

--- luxe/luxe.Server/Application/Services/ITokenService.cs
+++ luxe/luxe.Server/Application/Services/ITokenService.cs
@@ -1,13 +1,14 @@
 ﻿using luxe.Server.Application.DTOs;
 using luxe.Server.Application.DTOs.AuthenticationDTOs;
 using luxe.Server.Domain.Entities;
+using System.Security.Claims;
 
 namespace luxe.Server.Application.Services
 {
     public interface ITokenService
     {
-        // Task<ApiResponse<>> RegisterAsync(RegistrationRequestDTO registrationRequestDTO);
         string CreateAccessTokenAsync(AppUser user, IList<string> roles);
-        RefreshToken CreateRefreshToken(string ipAddress);   
+        RefreshToken CreateRefreshToken(string ipAddress);
+        ClaimsPrincipal? GetPrincipalFromExpiredToken(string token);
     }
 }
