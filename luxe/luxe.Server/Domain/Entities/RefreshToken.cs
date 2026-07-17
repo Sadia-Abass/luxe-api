b7774cdf39b1a4abd9b7ec93969a874706767b9b
Sadia Abass <sadia.abass@outlook.com>
2026-07-17 23:56:50 +0100

    Remove unneccessary properties and change Expires name to ExpireDate in RefreshToken entity class.

--- luxe/luxe.Server/Domain/Entities/RefreshToken.cs
+++ luxe/luxe.Server/Domain/Entities/RefreshToken.cs
@@ -3,19 +3,17 @@
     public class RefreshToken
     {
         public int Id { get; set; }
-        public string? Token { get; set; } = string.Empty;
-        public DateTime Expires { get; set; }
-        public DateTime CreatedDate { get; set; }
-        public string? CreatedByIp { get; set; }
+        public string Token { get; set; } = string.Empty;
+        public DateTime ExpiresDate { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
         public DateTime? RevokedDate { get; set; }
-        public string? RevokedByIp { get; set; }
         public string? ReplacedByToken { get; set; }
-
-        public bool IsExpired => DateTime.UtcNow >= Expires;
-        public bool IsActive => RevokedDate == null && !IsExpired;
 
         public string UserId { get; set; } = string.Empty;
         public AppUser User { get; set; } = null!;
 
+        // Helper properties - computed, not stored in DB
+        public bool IsExpired => DateTime.UtcNow >= ExpiresDate;
+        public bool IsActive => RevokedDate == null && !IsExpired;
     }
 }
