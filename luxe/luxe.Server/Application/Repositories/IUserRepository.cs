c93fe8b99e15b686309cb5cbc4087e3cd5d0877e
Sadia Abass <sadia.abass@outlook.com>
2026-07-18 02:49:30 +0100

    Add new methods.

--- luxe/luxe.Server/Application/Repositories/IUserRepository.cs
+++ luxe/luxe.Server/Application/Repositories/IUserRepository.cs
@@ -4,6 +4,11 @@ namespace luxe.Server.Application.Repositories
 {
     public interface IUserRepository
     {
+        Task<AppUser?> GetUserWithRefreshTokenAsync(string userId);
+        Task<AppUser?> GetUserByRefreshTokenAsync(string refreshToken);
+        Task<AppUser?> GetUserByEmailAsync(string email);
         Task SaveRefreshTokenAsync(RefreshToken refreshToken);
+        Task<RefreshToken?> GetRefreshTokenAsync(string refreshToken);
+        Task RevokeRefreshTokenAsync(RefreshToken refreshToken, string? replaceToken = null);
     }
 }
