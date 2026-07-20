fc22c608ede53d76a89d8bf00c376f0546e422c9
Sadia Abass <sadia.abass@outlook.com>
2026-07-21 00:07:40 +0100

    Add ResetPasswordDTO

---
+++ luxe/luxe.Server/Application/DTOs/AuthenticationDTOs/ResetPasswordDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace luxe.Server.Application.DTOs.AuthenticationDTOs
+{
+    public class ResetPasswordDTO
+    {
+        public string UserId { get; set; } = string.Empty;
+        public string Token { get; set; } = string.Empty;
+        public string NewPassword { get; set; } = string.Empty;
+    }
+}
