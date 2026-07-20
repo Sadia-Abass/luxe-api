cbd86dde765363f1a3ec62197375872c842a61f0
Sadia Abass <sadia.abass@outlook.com>
2026-07-21 00:49:30 +0100

    Add ChangePassowrdDTO

---
+++ luxe/luxe.Server/Application/DTOs/AuthenticationDTOs/ChangePasswordDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace luxe.Server.Application.DTOs.AuthenticationDTOs
+{
+    public class ChangePasswordDTO
+    {
+        public string CurrentPassword { get; set; } = string.Empty;
+        public string NewPassword { get; set; } = string.Empty;
+    }
+}
