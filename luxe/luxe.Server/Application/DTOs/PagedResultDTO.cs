915dc0bd6b572b89b1f83aefb498c463356d35aa
Sadia Abass <sadia.abass@outlook.com>
2026-07-22 00:59:53 +0100

    Add agedResultDTO

---
+++ luxe/luxe.Server/Application/DTOs/PagedResultDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace luxe.Server.Application.DTOs
+{
+    public class PagedResultDTO<T>
+    {
+        public IReadOnlyList<T> Items { get; set; } = new List<T>();
+        public int TotalCount { get; set; }
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public int TotalPages => (int)Math.Ceiling(TotalCount / (double)PageSize);
+    }
+}
