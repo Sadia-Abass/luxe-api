f669285fb99cab6795beba815e0a966a4b9873b9
Sadia Abass <sadia.abass@outlook.com>
2026-07-22 00:54:04 +0100

    Add UpdateUserDTO

---
+++ luxe/luxe.Server/Application/DTOs/Users/UpdateUserDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace luxe.Server.Application.DTOs.Users
+{
+    public class UpdateUserDTO
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+    }
+}
