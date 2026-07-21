7b110ff18330cbe4430a71cd09b22854fdd23f17
Sadia Abass <sadia.abass@outlook.com>
2026-07-22 00:20:05 +0100

    Add GenericRepository implementations.

---
+++ luxe/luxe.Server/Infrastructure/Repositories/GenericRepository.cs
@@ -0,0 +1,76 @@
+﻿using luxe.Server.Application.Repositories;
+using luxe.Server.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace luxe.Server.Infrastructure.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        protected readonly AppDbContext _appDbContext;
+        protected readonly DbSet<T> _dbSet;
+
+        public GenericRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+            _dbSet = appDbContext.Set<T>();
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+            await _appDbContext.SaveChangesAsync();
+        }
+
+        public async Task RemoveAsync(T entity)
+        {
+            _dbSet.Remove(entity);
+            await _appDbContext.SaveChangesAsync();
+        }
+
+        public async Task<IReadOnlyList<T>> FindAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _dbSet.Where(predicate).ToListAsync();
+        }
+
+        public async Task<IReadOnlyList<T>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<T?> GetByIdAsync(object id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+
+        public async Task<T?> SingleOrDefaultAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _dbSet.SingleOrDefaultAsync(predicate);
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _dbSet.Update(entity);
+            await _appDbContext.SaveChangesAsync();
+        }
+
+
+        public async Task<(IReadOnlyList<T> Items, int TotalCount)> GetPagedAsync(int pageNumber, int pageSize, Expression<Func<T, bool>>? filter = null)
+        {
+            var query = _dbSet.AsQueryable();
+            if(filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            var totalCount = await query.CountAsync();
+            var items = await query
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+
+            return (items, totalCount);
+        }
+    }
+}
