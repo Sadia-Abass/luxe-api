9a29f17035569d2d5b71c5b23a699ebde1ac3bb1
Sadia Abass <sadia.abass@outlook.com>
2026-07-22 00:19:35 +0100

    Add IGenericRepository interface.

---
+++ luxe/luxe.Server/Application/Repositories/IGenericRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq.Expressions;
+
+namespace luxe.Server.Application.Repositories
+{
+    public interface IGenericRepository<T> where T : class   
+    {
+        Task<T?> GetByIdAsync(object id);
+        Task<IReadOnlyList<T>> GetAllAsync();
+        Task<IReadOnlyList<T>> FindAsync(Expression<Func<T, bool>> predicate);
+        Task<T?> SingleOrDefaultAsync(Expression<Func<T, bool>> predicate);
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task RemoveAsync(T entity);
+        Task<(IReadOnlyList<T> Items, int TotalCount)> GetPagedAsync(int pageNumber, int pageSize, Expression<Func<T, bool>>? filter = null);
+    }
+}
