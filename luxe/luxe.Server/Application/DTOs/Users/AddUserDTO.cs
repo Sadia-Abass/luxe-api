4abe02e9cdbd6f49e69046599cb0508fa8f26879
Sadia Abass <sadia.abass@outlook.com>
2026-07-22 01:45:20 +0100

    Add AddUserDTO

---
+++ luxe/luxe.Server/Application/DTOs/Users/AddUserDTO.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace luxe.Server.Application.DTOs.Users
+{
+    public class AddUserDTO
+    {
+        public string Id { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public DateTime DOB { get; set; }
+        public IFormFile ImageUrl { get; set; } = null!;
+    }
+}
