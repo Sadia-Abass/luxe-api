2b733ebfcc8c242a36a83bb26fc64c41a99e29a3
Sadia Abass <sadia.abass@outlook.com>
2026-07-22 01:48:34 +0100

    Add GetAllUsersAsync,  GetUserByIdAsync, UpdateUserAsync, AddNewUser, UpdateProfileImageAsync, DeleteUserAsync, AssignRole, RemoveRole methods to IUserRepository

--- luxe/luxe.Server/Application/Repositories/IUserRepository.cs
+++ luxe/luxe.Server/Application/Repositories/IUserRepository.cs
@@ -1,4 +1,6 @@
-﻿using luxe.Server.Domain.Entities;
+﻿using luxe.Server.Application.DTOs;
+using luxe.Server.Application.DTOs.Users;
+using luxe.Server.Domain.Entities;
 
 namespace luxe.Server.Application.Repositories
 {
@@ -10,5 +12,14 @@ namespace luxe.Server.Application.Repositories
         Task SaveRefreshTokenAsync(RefreshToken refreshToken);
         Task<RefreshToken?> GetRefreshTokenAsync(string refreshToken);
         Task RevokeRefreshTokenAsync(RefreshToken refreshToken, string? replaceToken = null);
+
+        Task<ApiResponse<PagedResultDTO<UserResponseDTO>>> GetAllUsersAsync(int pageNumber, int pageSize, string? search);
+        Task<ApiResponse<UserResponseDTO>> GetUserByIdAsync(string id);
+        Task<ApiResponse<UserResponseDTO>> UpdateUserAsync(string userId, UpdateUserDTO updateUserDto);
+        Task<ApiResponse<UserResponseDTO>> AddNewUser(AddUserDTO addUserDTO);
+        Task<ApiResponse<string>> UpdateProfileImageAsync(string userId, IFormFile file);
+        Task<ApiResponse<string>> DeleteUserAsync(string userId);
+        Task<ApiResponse<string>> AssignRole(string userId, AssignRoleDTO assignRoleDTO);
+        Task<ApiResponse<string>> RemoveRole(string userId, string role);
     }
 }
